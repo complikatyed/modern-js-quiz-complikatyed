b6e9f15b2d10c662512b234c427c5d51e533a7e4
complikatyed <katye.russell@gmail.com>
2016-05-12 21:50:40 -0500

    Initial robot attribute planning

---
+++ scripts/robots.js
@@ -0,0 +1,18 @@
+// Robot Planning (with Matthew Russell's help)
+
+// Attributes:  attack, defense, speed, stamina
+// Offensive Technology (raises attack abilities): lasers, missiles, blades
+// Defensive Technology: forcefield (increases defense/blocks attack), smoke shield (slows other robots), sonar (detects attack)
+// Speed Technology: jet pack, turbo booster, supersonic blast
+// Stamina/Strength Technology:  the juicer, lightning bolt, iron man
+// When attack points increase, defense points are lowered
+// When defense increases, speed lowers
+// When speed increases, stamina lowers
+// When stamina increases, attack lowers
+
+// WHEN DEFENSIVE POINTS = 0, opponent's attack points increase by 10
+// WHEN ATTACK POINTS = 0, player loses
+// WHEN SPEED POINTS = 0, attack points decrease by 5
+// WHEN STAMINA POINTS = 0, player loses
+
+
